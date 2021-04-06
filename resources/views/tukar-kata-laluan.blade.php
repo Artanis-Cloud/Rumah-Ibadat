69dc8fa7a8c8c3f552854a9ad73d7f0d77357f1c
amsyarDev <amsyaraazhar97@gmail.com>
2021-04-06 17:41:37 +0800

    push
    - fix layout
    - fix dashboard
    - add change pass page

--- resources/views/tukar-kata-laluan.blade.php
+++ resources/views/tukar-kata-laluan.blade.php
@@ -1,123 +1,91 @@
-@extends('layouts.layout-user')
+@extends('layouts.layout-user-nicepage')
 
 @section('content')
-<div class="panel-header panel-header-sm">
-       
-</div>
-<div class="content">
-  <div class="row">
-    {{-- <div class="col-md-2"></div> --}}
-    <div class="col-md">
-      <div class="card">
-        {{-- @if (count($errors) > 0) --}}
-        @if ($message = Session::get('success'))
-          <div class="m-3 alert alert-success" style="text-align: center;">
-              {{$message}}
-              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-          </div>
-        @elseif ($message = Session::get('error')) 
-          <div class="m-3 alert alert-danger" style="text-align: center;">
-              {{$message}}
-              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-          </div>
-        @endif
-
-        <form method="POST" action="{{ route('tukar-kata-laluan.kemaskini') }}">
-        {{ csrf_field() }}
-        <div class="card-header" style="text-align: center;">
-          Tukar Kata Laluan
+<!-- ============================================================== -->
+<!-- Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<div class="page-breadcrumb">
+    <div class="row">
+        <div class="col-5 align-self-center">
+            <h4 class="page-title">Tukar Kata Laluan</h4>
         </div>
-
-        <div class="card-body ">
-
-            <div class="row"> 
-              <div class="col-md-2"></div>
-              <div class="col-md">
-                <div class="form-group has-label">
-                    <label>
-                        Kata Laluan Terdahulu
-                    </label>
-                    <input class="form-control" name="old_password" type="password" required="true">
-                </div>
-              </div>
-              <div class="col-md-2"></div>
+        <div class="col-7 align-self-center">
+            <div class="d-flex align-items-center justify-content-end">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="{{ route('user.halaman-utama') }}">Halaman Utama</a>
+                        </li>
+                        <li class="breadcrumb-item"> Profil </li>
+                        <li class="breadcrumb-item active" aria-current="page">Tukar Kata Laluan</li>
+                    </ol>
+                </nav>
             </div>
-
-
-            <div class="row"> 
-              <div class="col-md-2"></div>
-              <div class="col-md">
-                <div class="form-group has-label">
-                    <label>
-                        Kata Laluan Baru
-                    </label>
-                    <input class="form-control" name="password" type="password" required="true">
-                </div>
-              </div>
-              <div class="col-md-2"></div>
-            </div>
-
-
-            <div class="row"> 
-              <div class="col-md-2"></div>
-              <div class="col-md">
-                <div class="form-group has-label">
-                    <label>
-                        Sahkan Kata Laluan Baru
-                    </label>
-                    <input class="form-control" name="password_confirmation" type="password" required="true">
-                </div>
-              </div>
-              <div class="col-md-2"></div>
-            </div>
-
-
-
         </div>
-
-        <div class="text-center card-footer">
-          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal_pengesahan">Tukar Kata Laluan</button>
-        </div>
-
-        {{-- MODAL CONFIRMATION --}}
-        <div class="modal fade" id="modal_pengesahan" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Pengesahan</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                Anda pasti mahu menukar kata laluan baru?
-              </div>
-              <div class="modal-footer">
-                {{-- <button type="button" class="btn btn-danger" data-dismiss="modal">Batal</button> --}}
-                <button type="submit" class="btn btn-primary">Tukar</button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        </form>
-      </div>
-      
     </div>
-  </div>
 </div>
+<!-- ============================================================== -->
+<!-- End Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script type="text/javascript">
-$("document").ready(function(){
-  setTimeout(function(){
-     $("div.alert").remove();
-  }, 5000 ); // 5 secs
+<!-- ============================================================== -->
+<!-- Container fluid  -->
+<!-- ============================================================== -->
+<div class="container-fluid">
+  
+  <div class="row">
+      <div class="col-12">
+          <div class="card">
+              <div class="card-body">
 
-});
-</script>
+                  {{-- Flash Message --}}
+                  <div class="alert alert-success" style="text-align: center;">Kata laluan berjaya ditukar!</div>
+
+                  <div class="row"> 
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6">
+                      <div class="form-group has-label">
+                          <label>Kata Laluan Terdahulu</label>
+                          <input class="form-control" name="old_password" type="password" required="true">
+                          
+                      </div>
+                    </div>
+                    <div class="col-md-3"></div>
+                  </div>
+
+                  <div class="row"> 
+                    <div class="col-md-3"></div>
+                    <div class="col-md-3">
+                      <div class="form-group has-label">
+                          <label>Kata Laluan Baru</label>
+                          <input class="form-control" name="password" type="password" required="true">
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-group has-label">
+                          <label>Sahkan Kata Laluan Baru</label>
+                          <input class="form-control" name="password_confirmation" type="password" required="true">
+                      </div>
+                    </div>
+                    <div class="col-md-3"></div>
+                  </div>
+
+                  <div class="row"> 
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6" style="text-align: center;">
+                      <button type="button" class="btn waves-effect waves-light btn-info">Tukar Kata laluan</button>
+                    </div>
+                    <div class="col-md-3"></div>
+                  </div>
+
+              </div>
+          </div>
+      </div>
+  </div>
+
+</div>
+<!-- ============================================================== -->
+<!-- End Container fluid  -->
+<!-- ============================================================== -->
+
 @endsection
