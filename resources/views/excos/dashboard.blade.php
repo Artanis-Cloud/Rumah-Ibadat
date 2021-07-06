096f687ba9e476eb3b9cf795ee962a94bdf48c8d
amsyarDev <amsyaraazhar97@gmail.com>
2021-07-06 21:54:41 +0800

    push
    - dash board permohon complete

--- resources/views/excos/dashboard.blade.php
+++ resources/views/excos/dashboard.blade.php
@@ -11,7 +11,7 @@
 <!-- ============================================================== -->
 <div class="row">
     <div class="col-lg-3 col-md-6">
-        <a class="card bg-info" href="{{ route('excos.permohonan.baru') }}">
+        <a class="card card-hover bg-info" href="{{ route('excos.permohonan.baru') }}">
             <div class="card-body">
                 <div class="d-flex no-block align-items-center">
                     <div class="text-white">
@@ -26,7 +26,7 @@
         </a>
     </div>
     <div class="col-lg-3 col-md-6">
-        <a class="card bg-warning" href="{{ route('excos.permohonan.sedang-diproses') }}">
+        <a class="card card-hover bg-warning" href="{{ route('excos.permohonan.sedang-diproses') }}">
             <div class="card-body">
                 <div class="d-flex no-block align-items-center">
                     <div class="text-white">
@@ -41,7 +41,7 @@
         </a>
     </div>
     <div class="col-lg-3 col-md-6">
-        <a class="card bg-success" href="{{ route('excos.permohonan.lulus') }}">
+        <a class="card card-hover bg-success" href="{{ route('excos.permohonan.lulus') }}">
             <div class="card-body">
                 <div class="d-flex no-block align-items-center">
                     <div class="text-white">
@@ -56,7 +56,7 @@
         </a>
     </div>
     <div class="col-lg-3 col-md-6">
-        <a class="card bg-danger" href="{{ route('excos.permohonan.tidak-lulus') }}">
+        <a class="card card-hover bg-danger" href="{{ route('excos.permohonan.tidak-lulus') }}">
             <div class="card-body">
                 <div class="d-flex no-block align-items-center">
                     <div class="text-white">
@@ -71,114 +71,7 @@
         </a>
     </div>
 </div>
-<!-- ============================================================== -->
-<!-- Sales chart -->
-<!-- ============================================================== -->
-<!-- ============================================================== -->
-<!-- Email campaign chart -->
-<!-- ============================================================== -->
-{{-- Graph --}}
-{{-- <div class="row">
-    <div class="col-lg-3 col-md-6">
-        <div class="card">
-            <div class="card-body">
-                <h4 class="card-title">Campaign</h4>
-                <div id="campaign" style="height: 168px; width: 100%;" class="m-t-10"></div>
-                <!-- row -->
-                <div class="text-center row text-lg-left">
-                    <!-- column -->
-                    <div class="col-4">
-                        <h4 class="font-medium m-b-0">60%</h4>
-                        <span class="text-muted">Open</span>
-                    </div>
-                    <!-- column -->
-                    <div class="col-4">
-                        <h4 class="font-medium m-b-0">26%</h4>
-                        <span class="text-muted">Click</span>
-                    </div>
-                    <!-- column -->
-                    <div class="col-4">
-                        <h4 class="font-medium m-b-0">18%</h4>
-                        <span class="text-muted">Bounce</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title m-b-5">Referral Earnings</h5>
-                <h3 class="font-light">$769.08</h3>
-                <div class="text-center m-t-20">
-                    <div id="earnings"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-6 col-md-12 order-lg-2 order-md-3">
-        <div class="card">
-            <div class="card-body">
-                <div class="d-flex align-items-center">
-                    <div>
-                        <h4 class="card-title">Sales Ratio</h4>
-                    </div>
-                    <div class="ml-auto">
-                        <div class="dl m-b-10">
-                            <select class="border-0 custom-select text-muted">
-                                <option value="0" selected="">August 2018</option>
-                                <option value="1">May 2018</option>
-                                <option value="2">March 2018</option>
-                                <option value="3">June 2018</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-                <div class="d-flex align-items-center no-block">
-                    <div>
-                        <span class="text-muted">This Week</span>
-                        <h3 class="mb-0 font-light text-info">$23.5K <span class="text-muted font-12"><i class="mdi mdi-arrow-up text-success"></i>18.6</span></h3>
-                    </div>
-                    <div class="ml-4">
-                        <span class="text-muted">Last Week</span>
-                        <h3 class="mb-0 font-light text-muted">$945 <span class="text-muted font-12"><i class="mdi mdi-arrow-down text-danger"></i>46.3</span></h3>
-                    </div>
-                </div>
-                <div class="mt-5 sales ct-charts"></div>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-3 col-md-6 order-lg-3 order-md-2">
-        <div class="card">
-            <div class="card-body m-b-0">
-                <h4 class="card-title">Thursday <span class="font-normal font-14 text-muted">12th April, 2018</span></h4>
-                    <div class="flex-row d-flex align-items-center m-t-30">
-                    <h1 class="font-light"><i class="wi wi-day-sleet"></i> <span>35<sup>°</sup></span></h1>
-                </div>
-            </div>
-            <div class="weather-report" style="height:120px; width:100%;"></div>
-        </div>
-        <div class="card">
-            <div class="card-body">
-                <h4 class="card-title m-b-0">Users</h4>
-                <h2 class="font-light">35,658 <span class="font-medium font-16 text-success">+23%</span></h2>
-                <div class="m-t-30">
-                    <div class="text-center row">
-                        <div class="col-6 border-right">
-                            <h4 class="m-b-0">58%</h4>
-                            <span class="font-14 text-muted">New Users</span>
-                        </div>
-                        <div class="col-6">
-                            <h4 class="m-b-0">42%</h4>
-                            <span class="font-14 text-muted">Repeat Users</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> --}}
-<!-- ============================================================== -->
-<!-- Email campaign chart -->
-<!-- ============================================================== -->
+
 <!-- ============================================================== -->
 <!-- Ravenue - page-view-bounce rate -->
 <!-- ============================================================== -->
--- resources/views/users/permohonan/pilih.blade.php
+++ resources/views/users/permohonan/pilih.blade.php
@@ -11,61 +11,59 @@
   <div class="row">
       {{-- <div class="col-2"></div> --}}
       <div class="col-12">
-          {{-- <div class="card"> --}}
-            <div class="card-body">
+          
               <div class="row">
-              <div class="col-md">
-                <div class="border card border-info h-100">
-                  <div class="card-body">
-                      <h4 class="card-title" style="font-weight: bold;"><i class="fas fa-file-alt" style="color: #137eff;"></i>&nbsp&nbsp Permohonan Baru</h4>
-                      <p class="card-text">Sekiranya ingin membuat permohonan dana untuk rumah ibadat, sila pilih bahagian ini.</p>
+                <div class="col-md">
+                  <div class="border card card-hover border-info h-100">
+                    <div class="card-body">
+                        <h4 class="card-title" style="font-weight: bold;"><i class="fas fa-file-alt" style="color: #137eff;"></i>&nbsp&nbsp Permohonan Baru</h4>
+                        <p class="card-text">Sekiranya ingin membuat permohonan dana untuk rumah ibadat, sila pilih bahagian ini.</p>
+                    </div>
+                    <div class="card-footer">
+                      <a href="{{ route('users.permohonan.baru') }}" class="btn btn-info">Permohonan Baru</a>
+                    </div>
                   </div>
-                  <div class="card-footer">
-                    <a href="{{ route('users.permohonan.baru') }}" class="btn btn-info">Permohonan Baru</a>
+                </div>
+
+                <div class="col-md">
+                  <div class="border card card-hover border-info h-100">
+                    <div class="card-body">
+                        <h4 class="card-title" style="font-weight: bold;"><i class="fas fa-sync" style="color: yellow;"></i>&nbsp&nbsp Senarai Permohonan Sedang Diproses</h4>
+                        <p class="card-text">Sekiranya permohonan anda sedang diproses, sila pilih bahagian ini.</p>
+                    </div>
+                    <div class="card-footer">
+                      <a href="{{ route('users.permohonan.sedang-diproses') }}" class="btn btn-info">Senarai Permohonan Sedang Diproses</a>
+                    </div>
                   </div>
                 </div>
               </div>
 
-              <div class="col-md">
-                <div class="border card border-info h-100">
-                  <div class="card-body">
-                      <h4 class="card-title" style="font-weight: bold;"><i class="fas fa-sync" style="color: yellow;"></i>&nbsp&nbsp Senarai Permohonan Sedang Diproses</h4>
-                      <p class="card-text">Sekiranya permohonan anda sedang diproses, sila pilih bahagian ini.</p>
-                  </div>
-                  <div class="card-footer">
-                    <a href="{{ route('users.permohonan.sedang-diproses') }}" class="btn btn-info">Senarai Permohonan Sedang Diproses</a>
+              <div class="row" style="padding-top: 20px;">
+                <div class="col-md">
+                  <div class="border card card-hover border-info h-100">
+                    <div class="card-body">
+                        <h4 class="card-title" style="font-weight: bold;"><i class="far fa-check-circle" style="color: green;"></i>&nbsp&nbsp Senarai Permohonan Lulus</h4>
+                        <p class="card-text">Sekiranya permohonan anda lulus, sila pilih bahagian ini.</p>
+                    </div>
+                    <div class="card-footer">
+                      <a href="{{ route('users.permohonan.lulus') }}" class="btn btn-info">Senarai Permohonan Lulus</a>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
 
-            <div class="row" style="padding-top: 20px;">
-              <div class="col-md">
-                <div class="border card border-info h-100">
-                  <div class="card-body">
-                      <h4 class="card-title" style="font-weight: bold;"><i class="far fa-check-circle" style="color: green;"></i>&nbsp&nbsp Senarai Permohonan Lulus</h4>
-                      <p class="card-text">Sekiranya permohonan anda lulus, sila pilih bahagian ini.</p>
-                  </div>
-                  <div class="card-footer">
-                    <a href="{{ route('users.permohonan.lulus') }}" class="btn btn-info">Senarai Permohonan Lulus</a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md">
-                <div class="border card border-info h-100">
-                  <div class="card-body">
-                      <h4 class="card-title" style="font-weight: bold;"><i class="far fa-times-circle" style="color: red;"></i>&nbsp&nbsp Senarai Permohonan Tidak Lulus</h4>
-                      <p class="card-text">Sekiranya permohonan anda tidak lulus, sila pilih bahagian ini.</p>
-                  </div>
-                  <div class="card-footer">
-                    <a href="{{ route('users.permohonan.tidak-lulus') }}" class="btn btn-info">Senarai Permohonan Tidak Lulus</a>
+                <div class="col-md">
+                  <div class="border card card-hover border-info h-100">
+                    <div class="card-body">
+                        <h4 class="card-title" style="font-weight: bold;"><i class="far fa-times-circle" style="color: red;"></i>&nbsp&nbsp Senarai Permohonan Tidak Lulus</h4>
+                        <p class="card-text">Sekiranya permohonan anda tidak lulus, sila pilih bahagian ini.</p>
+                    </div>
+                    <div class="card-footer">
+                      <a href="{{ route('users.permohonan.tidak-lulus') }}" class="btn btn-info">Senarai Permohonan Tidak Lulus</a>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            </div>
-          {{-- </div> --}}
+            
       </div>
   </div>
 
