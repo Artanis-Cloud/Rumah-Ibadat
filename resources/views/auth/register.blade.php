14a00b3e19b7df08de19148bcab925e749da334b
luqmanulhakim96 <hakim9608@gmail.com>
2021-09-07 11:19:14 +0800

    add logo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -128,7 +128,7 @@
 <!-- ============================================================== -->
 <!-- End Container fluid  -->
 <!-- ============================================================== -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function() {
         $(document).on('submit', '#register_form', function() {
