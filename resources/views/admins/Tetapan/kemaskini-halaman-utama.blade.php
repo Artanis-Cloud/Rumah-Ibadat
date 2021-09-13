17a338a7bc2dc4bbbc24da59fd32c256158c5155
amsyarDev <amsyaraazhar97@gmail.com>
2021-09-13 17:36:53 +0800

    Update kemaskini-halaman-utama.blade.php

--- resources/views/admins/Tetapan/kemaskini-halaman-utama.blade.php
+++ resources/views/admins/Tetapan/kemaskini-halaman-utama.blade.php
@@ -18,7 +18,7 @@
 
                 <div class="row">
                   <div class="col-md">
-                    <h3 class="text-center">Pengenalan Sistem.</h3>
+                    <h3 class="text-center">Pengenalan Sistem</h3>
                   </div>
                 </div>
 
