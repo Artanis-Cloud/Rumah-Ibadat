13d4178b5073aafceec9a3440822cbdd700382ae
amsyarDev <amsyaraazhar97@gmail.com>
2021-10-05 15:51:37 +0800

    carousel bug fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -511,10 +511,206 @@
                         </p>
 
                     </div>
-                    <div class="col-md-8">
+
+                    @if ($banner->count() != 0)
+                        <div class="col-md-8">
+
+                            <!-- Slideshow container -->
+                            <style>
+                                * {
+                                    box-sizing: border-box
+                                }
+
+                                /* Slideshow container */
+                                .slideshow-container {
+                                    max-width: 1000px;
+                                    position: relative;
+                                    margin: auto;
+                                }
+
+                                /* Hide the images by default */
+                                .mySlides {
+                                    display: none;
+                                }
+
+                                /* Next & previous buttons */
+                                .prev,
+                                .next {
+                                    cursor: pointer;
+                                    position: absolute;
+                                    top: 50%;
+                                    width: auto;
+                                    margin-top: -22px;
+                                    padding: 16px;
+                                    color: white;
+                                    font-weight: bold;
+                                    font-size: 18px;
+                                    transition: 0.6s ease;
+                                    border-radius: 0 3px 3px 0;
+                                    user-select: none;
+                                }
+
+                                /* Position the "next button" to the right */
+                                .next {
+                                    right: 0;
+                                    border-radius: 3px 0 0 3px;
+                                }
+
+                                /* On hover, add a black background color with a little bit see-through */
+                                .prev:hover,
+                                .next:hover {
+                                    background-color: rgba(0, 0, 0, 0.8);
+                                }
+
+                                /* Caption text */
+                                .text {
+                                    color: #f2f2f2;
+                                    font-size: 15px;
+                                    padding: 8px 12px;
+                                    position: absolute;
+                                    bottom: 8px;
+                                    width: 100%;
+                                    text-align: center;
+                                }
+
+                                /* Number text (1/3 etc) */
+                                .numbertext {
+                                    color: #f2f2f2;
+                                    font-size: 12px;
+                                    padding: 8px 12px;
+                                    position: absolute;
+                                    top: 0;
+                                }
+
+                                /* The dots/bullets/indicators */
+                                .dot {
+                                    cursor: pointer;
+                                    height: 15px;
+                                    width: 15px;
+                                    margin: 0 2px;
+                                    background-color: #bbb;
+                                    border-radius: 50%;
+                                    display: inline-block;
+                                    transition: background-color 0.6s ease;
+                                }
+
+                                .active,
+                                .dot:hover {
+                                    background-color: #717171;
+                                }
+
+                                /* Fading animation */
+                                .fade {
+                                    -webkit-animation-name: fade;
+                                    -webkit-animation-duration: 1.5s;
+                                    animation-name: fade;
+                                    animation-duration: 1.5s;
+                                }
+
+                                @-webkit-keyframes fade {
+                                    from {
+                                        opacity: .4
+                                    }
+
+                                    to {
+                                        opacity: 1
+                                    }
+                                }
+
+                                @keyframes fade {
+                                    from {
+                                        opacity: .4
+                                    }
+
+                                    to {
+                                        opacity: 1
+                                    }
+                                }
+
+                            </style>
+                            <div class="slideshow-container">
+
+                                <!-- Full-width images with number and caption text -->
+                                @foreach ($banner as $key => $data2)
+
+                                    @if ($loop->first)
+
+                                        <div class="mySlides">
+                                            <div class="numbertext">{{ $key + 1 }}</div>
+                                            <img src="{{ asset($image_path = str_replace('public', 'storage', $data2->url)) }}"
+                                                style="width:100%; height: 300px !important;">
+                                            {{-- <div class="text">Caption Text</div> --}}
+                                        </div>
+
+                                    @else
+                                        <div class="mySlides">
+                                            <div class="numbertext">{{ $key + 1 }}</div>
+                                            <img src="{{ asset($image_path = str_replace('public', 'storage', $data2->url)) }}"
+                                                style="width:100%; height: 300px !important;">
+                                            {{-- <div class="text"></div> --}}
+                                        </div>
+                                    @endif
+
+                                @endforeach
+
+                                <!-- Next and previous buttons -->
+                                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                            </div>
+                            <br>
+
+                            <div style="text-align:center">
+                                @foreach ($banner as $key => $data2)
+                                    <span class="dot" onclick="currentSlide({{ $key + 1 }})"></span>
+                                @endforeach
+                            </div>
+
+                            <script>
+                                var slideIndex = 1;
+                                showSlides(slideIndex);
+
+                                // Next/previous controls
+                                function plusSlides(n) {
+                                    showSlides(slideIndex += n);
+                                }
+
+                                // Thumbnail image controls
+                                function currentSlide(n) {
+                                    showSlides(slideIndex = n);
+                                }
+
+                                function showSlides(n) {
+                                    var i;
+                                    var slides = document.getElementsByClassName("mySlides");
+                                    var dots = document.getElementsByClassName("dot");
+                                    if (n > slides.length) {
+                                        slideIndex = 1
+                                    }
+                                    if (n < 1) {
+                                        slideIndex = slides.length
+                                    }
+                                    for (i = 0; i < slides.length; i++) {
+                                        slides[i].style.display = "none";
+                                    }
+                                    for (i = 0; i < dots.length; i++) {
+                                        dots[i].className = dots[i].className.replace(" active", "");
+                                    }
+                                    slides[slideIndex - 1].style.display = "block";
+                                    dots[slideIndex - 1].className += " active";
+                                }
+                            </script>
+                        </div>
+                    @else
+                        <div class="col-md-8">
+                            <img src="https://www.selangor.gov.my/selangor/modules_resources/images_contents/de97bd80bcc8acfff39f2530972ddddf.jpg"
+                                alt="Default Poster" style="width:100%; height: 300px !important;">
+                        </div>
+                    @endif
+
+
+                    {{-- <div class="col-md-8">
                         @if ($banner->count() != 0)
                             <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                                <!-- Indicators -->
                                 <ol class="carousel-indicators">
                                     @foreach ($banner as $key => $data2)
                                         @if ($loop->first)
@@ -526,7 +722,6 @@
                                     @endforeach
                                 </ol>
 
-                                <!-- Wrapper for slides -->
                                 <div class="carousel-inner">
 
                                     @foreach ($banner as $key => $data2)
@@ -546,7 +741,6 @@
 
 
 
-                                <!-- Left and right controls -->
                                 <a class="left carousel-control" href="#myCarousel" data-slide="prev">
                                     <span class="glyphicon glyphicon-chevron-left"></span>
                                     <span class="sr-only">Previous</span>
@@ -560,7 +754,7 @@
                             <img src="https://www.selangor.gov.my/selangor/modules_resources/images_contents/de97bd80bcc8acfff39f2530972ddddf.jpg"
                                 alt="Default Poster" style="width:100%; height: 300px !important;">
                         @endif
-                    </div>
+                    </div> --}}
 
                 </div>
 
