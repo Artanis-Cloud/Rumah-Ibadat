9d0c363bc1f6351a8f6e5fad8121ccca5b8f5758
Amirul Amsyar <61406633+amsyarDev@users.noreply.github.com>
2022-02-11 17:07:22 +0800

    update seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -70,28 +70,56 @@ class UserSeeder extends Seeder
         );
 
 
+        // DB::table('peruntukans')->insert(
+        //     [
+        //         'total_fund' => '1000000',
+        //         'current_fund' => '0.00',
+        //         'balance_fund' => '1000000',
+
+
+        //         'total_tokong' => '400000',
+        //         'current_tokong' => '0.00',
+        //         'balance_tokong' => '400000',
+
+        //         'total_kuil' => '200000',
+        //         'current_kuil' => '0.00',
+        //         'balance_kuil' => '200000',
+
+        //         'total_gurdwara' => '200000',
+        //         'current_gurdwara' => '0.00',
+        //         'balance_gurdwara' => '200000',
+
+        //         'total_gereja' => '200000',
+        //         'current_gereja' => '0.00',
+        //         'balance_gereja' => '200000',
+
+        //         'created_at' => $current_date,
+        //         'updated_at' => $current_date,
+        //     ]
+        // );
+
         DB::table('peruntukans')->insert(
             [
-                'total_fund' => '1000000',
+                'total_fund' => '1',
                 'current_fund' => '0.00',
-                'balance_fund' => '1000000',
+                'balance_fund' => '1',
 
 
-                'total_tokong' => '400000',
+                'total_tokong' => '1',
                 'current_tokong' => '0.00',
-                'balance_tokong' => '400000',
+                'balance_tokong' => '1',
 
-                'total_kuil' => '200000',
+                'total_kuil' => '1',
                 'current_kuil' => '0.00',
-                'balance_kuil' => '200000',
+                'balance_kuil' => '1',
 
-                'total_gurdwara' => '200000',
+                'total_gurdwara' => '1',
                 'current_gurdwara' => '0.00',
-                'balance_gurdwara' => '200000',
+                'balance_gurdwara' => '1',
 
-                'total_gereja' => '200000',
+                'total_gereja' => '1',
                 'current_gereja' => '0.00',
-                'balance_gereja' => '200000',
+                'balance_gereja' => '1',
 
                 'created_at' => $current_date,
                 'updated_at' => $current_date,
@@ -101,7 +129,7 @@ class UserSeeder extends Seeder
         $hashed_random_password = Hash::make("1234567890");
 
         // DB::table('users')->delete();
-        DB::table('users')->insert(
+        DB::table(' ')->insert(
             [
                 'id' => '1',
                 'role' => '4',
@@ -116,78 +144,78 @@ class UserSeeder extends Seeder
             ]
         );
 
-        DB::table('users')->insert(
-            [
-                'id' => '2',
-                'role' => '3',
-                'status' => '1',
-                'is_firstime' => '0',
-                'is_rumah_ibadat' => '0',
-                'name' => 'Mohammad Noorshahid bin Sharifudin',
-                'email' => 'upen@selangor.gov.my',
-                'ic_number' => '333333333333',
-                'mobile_phone' => '0123456789',
-                'password' => $hashed_random_password,
-            ]
-        );
+        // DB::table('users')->insert(
+        //     [
+        //         'id' => '2',
+        //         'role' => '3',
+        //         'status' => '1',
+        //         'is_firstime' => '0',
+        //         'is_rumah_ibadat' => '0',
+        //         'name' => 'Mohammad Noorshahid bin Sharifudin',
+        //         'email' => 'upen@selangor.gov.my',
+        //         'ic_number' => '333333333333',
+        //         'mobile_phone' => '0123456789',
+        //         'password' => $hashed_random_password,
+        //     ]
+        // );
 
-        DB::table('users')->insert(
-            [
-                'id' => '3',
-                'role' => '2',
-                'status' => '1',
-                'is_firstime' => '0',
-                'is_rumah_ibadat' => '0',
-                'name' => 'Muhd Luqman Nul Hakim Bin Abdul Hadi',
-                'email' => 'yb@selangor.gov.my',
-                'ic_number' => '222222222222',
-                'mobile_phone' => '0123456789',
-                'password' => $hashed_random_password,
-            ]
-        );
+        // DB::table('users')->insert(
+        //     [
+        //         'id' => '3',
+        //         'role' => '2',
+        //         'status' => '1',
+        //         'is_firstime' => '0',
+        //         'is_rumah_ibadat' => '0',
+        //         'name' => 'Muhd Luqman Nul Hakim Bin Abdul Hadi',
+        //         'email' => 'yb@selangor.gov.my',
+        //         'ic_number' => '222222222222',
+        //         'mobile_phone' => '0123456789',
+        //         'password' => $hashed_random_password,
+        //     ]
+        // );
 
-        DB::table('user_roles')->insert(
-            [
-                'user_id' => '3',
+        // DB::table('user_roles')->insert(
+        //     [
+        //         'user_id' => '3',
 
-                'tokong' => '1',
-                'kuil' => '0',
-                'gurdwara' => '0',
-                'gereja' => '0',
-            ]
-        );
+        //         'tokong' => '1',
+        //         'kuil' => '0',
+        //         'gurdwara' => '0',
+        //         'gereja' => '0',
+        //     ]
+        // );
 
-        DB::table('users')->insert(
-            [
-                'id' => '4',
-                'role' => '1',
-                'status' => '1',
-                'is_firstime' => '0',
-                'is_rumah_ibadat' => '0',
-                'name' => 'Mohd Hafiz Bin Ghiyatuddin',
-                'email' => 'exco@selangor.gov.my',
-                'ic_number' => '111111111111',
-                'mobile_phone' => '0123456789',
-                'password' => $hashed_random_password,
-            ]
-        );
+        // DB::table('users')->insert(
+        //     [
+        //         'id' => '4',
+        //         'role' => '1',
+        //         'status' => '1',
+        //         'is_firstime' => '0',
+        //         'is_rumah_ibadat' => '0',
+        //         'name' => 'Mohd Hafiz Bin Ghiyatuddin',
+        //         'email' => 'exco@selangor.gov.my',
+        //         'ic_number' => '111111111111',
+        //         'mobile_phone' => '0123456789',
+        //         'password' => $hashed_random_password,
+        //     ]
+        // );
 
-        DB::table('user_roles')->insert(
-            [
-                'user_id' => '4',
+        // DB::table('user_roles')->insert(
+        //     [
+        //         'user_id' => '4',
 
-                'tokong' => '1',
-                'kuil' => '0',
-                'gurdwara' => '0',
-                'gereja' => '0',
-            ]
-        );
+        //         'tokong' => '1',
+        //         'kuil' => '0',
+        //         'gurdwara' => '0',
+        //         'gereja' => '0',
+        //     ]
+        // );
 
         // \App\Models\User::factory(20)
         // ->create();
 
-        \App\Models\RumahIbadat::factory(50)
-        ->create();
+        // \App\Models\RumahIbadat::factory(50)
+        // ->create();
 
         // \App\Models\Permohonan::factory(20)
         // ->create();
